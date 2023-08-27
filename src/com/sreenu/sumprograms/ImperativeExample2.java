296ec5c0f11de29a07f187d9274528dc615171c9
Derangula Vijay <derangulavijay2011@gmail.com>
2023-08-27 12:16:20 +0530

    unused import

--- src/com/sreenu/sumprograms/ImperativeExample2.java
+++ src/com/sreenu/sumprograms/ImperativeExample2.java
@@ -3,7 +3,6 @@ package com.sreenu.sumprograms;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class ImperativeExample2 {
